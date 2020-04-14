2e8bb903e209a55621967e7ba75c13a70cc94d49
Anna Joe <57370408+Annajoe96@users.noreply.github.com>
2020-04-14 16:17:49 +0100

    Update calculator.rb

--- WIP/calculator.rb
+++ WIP/calculator.rb
@@ -6,6 +6,7 @@ class CheckoutSummary
   def initialize(**args)
     @accumulate = args[:accumulate] || args[:accumulate] == nil
     @discounts = {}
+    @gross_cost = args[:gross_cost]
   end
 
   def add_discount(**args)
@@ -43,42 +44,60 @@ class CheckoutSummary
   end
 
   
-  def calculate_discount_accumulate_true(fullcost)
-    cost = fullcost 
+  def calculate_discount_accumulate_true
+    cost = @gross_cost
     discounted_amounts = []
+    discount_breakdown = []
 
     @discounts.each do |_key,discount|
-      return 0 if discount[:amount].to_i > fullcost || discount[:percentage].to_i == 100
+      if discount[:amount].to_i > @gross_cost || discount[:percentage].to_i == 100
+        return [{name: discount[:name], discount: cost}, {net_amount: 0}]
+      end
       if discount[:percentage]
         percentage_off = cost * (discount[:percentage] / 100.to_f)
       end
 
       discounted_amounts =  [discount[:amount], percentage_off].compact.max
+
+      discount_breakdown << ({
+        name: discount[:name],
+        discount: discounted_amounts
+      })
+
       cost = cost - discounted_amounts
     end
-    cost
+    discount_breakdown << ({net_amount: cost})
   end
 
-  def calculate_discount_accumulate_false(cost)
+  def calculate_discount_accumulate_false
     discounted_amounts = []
+    discount_breakdown = []
 
     @discounts.each do |_key, discount|
-      return 0 if discount[:amount].to_i >= cost || discount[:percentage].to_i >= 100
+     if discount[:amount].to_i > @gross_cost || discount[:percentage].to_i == 100
+        return [{name: discount[:name], discount: @gross_cost}, {net_amount: 0}]
+      end
 
       if discount[:percentage]
-        percentage_off = cost * (discount[:percentage] / 100.to_f)
+        percentage_off = @gross_cost * (discount[:percentage] / 100.to_f)
       end
 
       discounted_amounts << [discount[:amount], percentage_off].compact.max
+      discount_breakdown << {
+        name: discount[:name],
+        discount: discounted_amounts.last
+      }
     end
-    cost - discounted_amounts.sum
+    gross_cost = @gross_cost - discounted_amounts.sum
+    discount_breakdown << ({net_amount: gross_cost})
+    
   end
 
-  def calculate_discount(fullcost)
+  def calculate
     if @accumulate  
-      calculate_discount_accumulate_true(fullcost)
+      calculate_discount_accumulate_true
     else
-      calculate_discount_accumulate_false(fullcost)
+      calculate_discount_accumulate_false
     end
   end
 
